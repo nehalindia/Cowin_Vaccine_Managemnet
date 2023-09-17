4a1f6d450e4626c60a391bc01e5f3e285326f315
kazmy <nehaluddin.dpe@gmail.com>
2023-09-18 01:30:32 +0530

    readme

---
+++ readme.md
@@ -0,0 +1,1 @@
+<h1>Vaccine App</h1>
--- src/route/route.js
+++ src/route/route.js
@@ -3,11 +3,12 @@ const router = express.Router()
 
 const { registerUser, userLogin} = require('../controller/userController')
 const { adminLogin, getUser} = require('../controller/adminController')
+const { authorize } = require('../middleware/auth')
 
 router.post('/register', registerUser)
 router.post('/login', userLogin)
 
 router.post('/adminlogin', adminLogin)
-router.get('/getuser',getUser)
+router.get('/getuser', authorize, getUser)
 
 module.exports = router
