fe2b977c8c3affef40789512f909a538050a0477
kazmy <nehaluddin.dpe@gmail.com>
2023-09-18 00:39:31 +0530

    User Controller created

---
+++ src/controller/userController.js
@@ -0,0 +1,135 @@
+const userModel = require('../model/userModel')
+const { isValid,isValidRequestBody } = require('../validation/validator');
+const validator = require('validator');
+const aadhaar_validator = require('aadhaar-validator')
+const jwt = require('jsonwebtoken')
+require('dotenv').config
+
+
+const registerUser = async function(req,res){
+    try{
+      
+        if(!isValidRequestBody(req.body)){
+            return res.status(400).send({status :false, message: "Must add data"})
+        }
+
+        let {Name, PhoneNumber, Age, Pincode, Aadhaar, password} = req.body
+
+        if( !isValid(Name) || !isValid(PhoneNumber) || !isValid(Age) && typeof Age !== 'number' || 
+        !isValid(Pincode) && typeof Pincode !== 'number' || !isValid(Aadhaar) || !isValid(password) ){
+            return res.status(400).send({
+                status: false,
+                message : "Enter valid detail"
+            })
+        }
+
+        password = password.trim()
+        if(password.length < 8 || password.length > 15){
+            return res.status(400).send({
+                status: false,
+                message: "Password length must between 8 to 15"
+            })
+        }
+
+        PhoneNumber = PhoneNumber.trim()
+        if(!validator.isMobilePhone(PhoneNumber)){
+            return res.status(400).send({
+                status : false,
+                message : "Enter valid Mobile Number"
+            })
+        }
+
+        const phoneExist = await userModel.findOne({PhoneNumber})     
+        if(phoneExist){
+            return res.status(400).send({
+              status : false, 
+              message : "Phone Number already registerd" 
+            })
+        }
+
+        if(Aadhaar.length !== 12){
+            return res.status(400).send({
+                status: false,
+                message: "Enter Valid Aadhaar Number"
+            })
+        }
+
+        // if(!aadhaar_validator.isValidNumber(Aadhaar)){
+        //     return res.status(400).send({
+        //         status: false,
+        //         message: "Enter Valid Aadhaar Number"
+        //     })
+        // }
+
+        const user = await userModel.create(req.body)
+
+        res.status(201).send({
+            status: true,
+            message: "user Register successfully",
+            data: user
+        })
+
+    }catch(error){
+        res.status(500).send({
+            status : false, 
+            message : error.message
+        })
+    }
+}
+
+
+const userLogin = async (req, res) => {
+    try{
+        if(!isValidRequestBody(req.body)){
+            return res.status(400).send({status :false, message: "Must add data"})
+        }
+        
+        let {PhoneNumber, password}= req.body
+        
+        if(!isValid(PhoneNumber) || !isValid(password)){
+            return res.status(400).send({
+                status: false,
+                message: "please provide valid phoneNumber and password"
+            })
+        }
+
+        if(!validator.isMobilePhone(PhoneNumber)){
+            return res.status(400).send({
+                status : false,
+                message : "Enter valid Mobile Number"
+            })
+        }
+
+        const user = await userModel.findOne({PhoneNumber})
+        if(!user){
+            return res.status(401).send({
+              status : false, 
+              message : "user Not Registerd" 
+            })
+        }
+
+        const token = jwt.sign({userId : user._id}, process.env.JWT_SECRET_KEY,{expiresIn : "25d"},
+        {iat : Date.now()})
+        
+        res.status(200).send({
+          status : true,
+          message: "user login successful", 
+          data : {
+            userId: user._id,
+            token: token
+            } 
+        })
+
+    }catch(error){
+        res.status(500).send({
+            status : false, 
+            message : error.message
+        })
+    }
+}
+
+module.exports = {
+    registerUser,
+    userLogin,
+
+}
--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const { registerUser, userLogin} = require('../controller/userController')
 
+router.post('/register', registerUser)
+router.post('/login', userLogin)
 
 module.exports = router
